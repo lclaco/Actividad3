34fd148a67c55e5eee995021bfc003b79bfb5822
lclaco <leonelclavijo@gmail.com>
2022-03-31 22:44:18 -0300

    Add files via upload

--- evento/src/evento/AsientoTest.java
+++ evento/src/evento/AsientoTest.java
@@ -2,32 +2,32 @@ package evento;
 
 import static org.junit.jupiter.api.Assertions.*;
 
-import org.junit.jupiter.api.AfterAll;
-import org.junit.jupiter.api.AfterEach;
-import org.junit.jupiter.api.BeforeAll;
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 class AsientoTest {
 
-	@BeforeAll
-	static void setUpBeforeClass() throws Exception {
-	}
-
-	@AfterAll
-	static void tearDownAfterClass() throws Exception {
-	}
-
-	@BeforeEach
-	void setUp() throws Exception {
-	}
-
-	@AfterEach
-	void tearDown() throws Exception {
+	@Test
+	void testLlenarEvento() {
+		fail("Not yet implemented");
 	}
 
 	@Test
-	void test() {
+	void testMostrarSala() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testVerificarVoleto() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testComprarEntrada() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testEstadisticas() {
 		fail("Not yet implemented");
 	}
 
